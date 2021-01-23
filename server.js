4f522c9f0b4d0bf50c4d9828c3b2bceae04f6fa4
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-01-23 11:15:46 -0800

    Moved app express before calling PORT.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
+const app = express();
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -13,7 +14,6 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Creating express app and configuring middleware needed for authentication
-const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
