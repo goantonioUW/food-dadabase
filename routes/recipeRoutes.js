eae5e34b7ce1845ca28565ac2acf903827b54144
mfrisch87 <mikey.frisch@gmail.com>
2021-01-23 11:34:20 -0800

    started recipeRoutes.js

---
+++ routes/recipeRoutes.js
@@ -0,0 +1,24 @@
+const { Recipe } = require("../models");
+const route = require("express").Router();
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+const router = require("../controllers/food_controller");
+
+route.get("/api/recipes", isAuthenticated, (req, res) => {
+    Recipe.findAll({
+        where {
+            UserId: req.user.id
+        }
+    });
+});
+
+router.post("/api/recipes", isAuthenticated (req, res) => {
+    Recipe.create({
+        name: req.body.name,
+        UserId: req.user.id
+    }).then(newRecipe => {
+        res.json(newRecipe);
+    });
+});
+
+
+module.exports = router;
