ca4fa1822805975d066d950c44645b92d9c4ee23
Ben Domschot <Bdomschot@gmail.com>
2021-01-25 15:00:08 -0800

    fixed html routing, server.js removed from routes, html files deleted.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.redirect("/login");
   });
 
   app.get("/login", (req, res) => {
@@ -18,12 +18,20 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("landing");
+  });
+
+  app.get("/signup", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.render("landing2");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("index");
   });
 };
