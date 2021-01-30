7e31edfffd9b8cdb7739c513b470c7c0baf396ba
mfrisch87 <mikey.frisch@gmail.com>
2021-01-30 09:23:34 -0800

    moving list routes to separate file for cleanliness.

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,20 +1,20 @@
 const { List } = require("../models");
 const router = require("express").Router();
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-
+// Store List
 router.get("/api/list", isAuthenticated, (req, res) => {
   List.findAll({
     where: {
-      UserId: req.user.id
+      username: req.user.email
     }
   });
   return res;
 });
-
+// Add ingredient to list
 router.post("/api/list", isAuthenticated, (req, res) => {
   List.create({
     name: req.body.name,
-    UserId: req.user.id
+    username: req.user.email
   }).then(newIngredient => {
     res.json(newIngredient);
   });
